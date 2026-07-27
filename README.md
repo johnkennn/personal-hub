1619bbd2e0b59ed136375ccc6326ddb14b895f4b
zhengzixing <zhengzixing@sensetime.com>
2026-07-27 18:31:19 +0800

    chore: initialize project structure

---
+++ README.md
@@ -0,0 +1,30 @@
+# Personal Hub
+
+## 项目目标
+
+学习完整互联网项目开发流程。
+
+包括：
+
+- React
+- Spring Boot
+- MySQL
+- Redis
+- Docker
+- Linux
+- Nginx
+- CI/CD
+
+## 项目结构
+
+frontend/
+
+React 前端
+
+backend/
+
+Spring Boot 后端
+
+docs/
+
+项目文档
---
+++ backend/src/test/java/com/zzh/personal_hub/PersonalHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzh.personal_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
